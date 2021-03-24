3a2123d11aa8c8c6e16f6af8c6d5f10fd1c6eb9f
Christian V <viray.christianj@gmail.com>
2021-03-24 03:45:39 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -2,7 +2,7 @@ class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.2]
   
   def change
       add_column :student, :grade, :integer
-      add_column :student, :birthdate, :datetime
+      add_column :student, :birthdate, :string
     end
   end 
   
